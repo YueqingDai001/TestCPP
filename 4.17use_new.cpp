14c6d35569704d32a96ae8bca89d7a48c309bc93
YueqingDai001 <bp66290071@126.com>
2023-08-21 15:17:23 +0800

    use new

--- 4.17use_new.cpp
+++ 4.17use_new.cpp
@@ -5,4 +5,23 @@ int main()
 	int nights = 1001;
 	int * pt = new int;
 	*pt = 1001;
+
+	cout << "nights value = ";
+	cout << nights << ":location " << &nights <<endl;
+	cout << "int ";
+	cout << "value = " << *pt << ": location = " << pt  <<endl;
+	double * pd = new double;
+	*pd = 10000001.0;
+
+	cout << "double ";
+	cout << "value = " << *pt << ": location = " << pt <<endl;
+	cout << "location of pointer pd :" << &pd << endl;
+	cout << "size of pt = " << sizeof(pt);
+	cout << ": size of *pt = " << sizeof(*pt) << endl;
+	cout << "size of pd = " <<sizeof pd ;
+        :q
+
+
+	cout << ": size of *pd = " << sizeof(*pd) << endl;
+	return 0;
 }
